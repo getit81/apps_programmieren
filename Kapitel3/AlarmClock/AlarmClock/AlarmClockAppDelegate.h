0bfb3bdd4c171c892cbe651b4b53734f1dd3d161
DB <dbruenner@Davids-MacBook-Pro.local>
2014-09-13 17:13:39 +0200

    Initial Commit

---
+++ Kapitel3/AlarmClock/AlarmClock/AlarmClockAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AlarmClockAppDelegate.h
+//  AlarmClock
+//
+//  Created by David Brünner on 13.09.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlarmClockAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Kapitel3/AlarmClock/AlarmClock/AlarmClockViewController.m
@@ -0,0 +1,29 @@
+//
+//  AlarmClockViewController.m
+//  AlarmClock
+//
+//  Created by David Brünner on 13.09.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "AlarmClockViewController.h"
+
+@interface AlarmClockViewController ()
+
+@end
+
+@implementation AlarmClockViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
